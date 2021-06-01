318a81b3b198ac6e45b98acc6bc32b0cafcc6ce6
Julius <80947873+Juliu51@users.noreply.github.com>
2021-06-01 20:22:12 +0300

    api

--- js/axios.js
+++ js/axios.js
@@ -3,26 +3,39 @@ const config = { header : { Accept: 'application/json'}}
 // Make a request for a user with a given ID
 axios.get('https://api.cryptonator.com/api/full/btc-usd' , config)
   .then(function (response) {
-      let BTCbody = document.querySelector('.btc');
-      let keiciu = "";
-      let keitykla = response.data.ticker.markets;
+      let BTCbody = document.querySelector('.BTC');
+    BTCbody.innerHTML = (response.data.ticker.base)
+    //-----------------BTC-----------
+    let USDbody = document.querySelector('.USD');
+    USDbody.innerHTML = (response.data.ticker.target)
+    //-----------USD------------
+    let PRICEbody = document.querySelector('.PRICE');
+    PRICEbody.innerHTML = response.data.ticker.price
+    //----------PRICE----------------------
     
-      for (let i = 0; i < keitykla.length; i++) {
-          console.log(keiciu)
-          keiciu += `<div>${keitykla[i].market
-              }</div>`;
-          HTML = `<div>${response.data.ticker.base}</div>
-    <div>${response.data.ticker.target}</div>
-    <div>${response.data.ticker.price}</div>
-    ${keiciu}`;
-       
-          console.log(HTML)
+      let MARKETSbody = document.querySelector('.MARKETS');
+      let nice = "";
+      console.log(response.data.ticker.markets.length)
+      for (i = 0; i < response.data.ticker.markets.length; i++) {
+        nice += ((response.data.ticker.markets[i].market) + ' ' + (response.data.ticker.markets[i].price) + "\n");
+              MARKETSbody.innerHTML = nice;
       }
-      return HTML;
-      BTCbody.innerHTML = HTML
+      //----------------MARKETS---------------
+      // let PRICESbody = document.querySelector('.PRICES');
+      // let nice1 = "";
+      // console.log(response.data.ticker.markets.length)
+      // for (i = 0; i < response.data.ticker.markets.length; i++) {
+      //   nice1 += JSON.stringify(response.data.ticker.markets[i].price)
+        
+      //   PRICESbody.innerHTML = nice1;
+      //   //---------------PRICES--------------------
+      
+    
+    
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
+
 
